1104d42d6c46d45b5e14604926e3db7f997b817a
loicalleyne <loicalleyne@gmail.com>
2023-05-25 18:33:45 -0400

    commandmap fix

--- cli/commandmap.go
+++ cli/commandmap.go
@@ -3,17 +3,19 @@ package cli
 import "github.com/loicalleyne/cli/command"
 
 func CreateCommandMap(cli *Cli) map[string]func(args []string) {
-	return commandsToMap(cli.Commands)
+	m := commandsToMap(cli.Commands, "")
+	return m
 }
 
-func commandsToMap(commands []command.Command) map[string]func(args []string) {
+func commandsToMap(commands []command.Command, prefix string) map[string]func(args []string) {
 	commandMap := make(map[string]func(args []string))
 	for _, command := range commands {
-		commandMap[command.Name] = func(args []string) {
+		key := prefix + command.Name
+		commandMap[key] = func(args []string) {
 			command.Func(args)
 		}
 		if len(command.SubCommands) > 0 {
-			nestedCommandMap := commandsToMap(command.SubCommands)
+			nestedCommandMap := commandsToMap(command.SubCommands, key+"+")
 			commandMap = mergeMaps(commandMap, nestedCommandMap)
 		}
 	}
