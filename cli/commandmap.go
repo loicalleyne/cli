173fa874d5e7634428fa85d4bfc217c6d4f00c5e
loicalleyne <loicalleyne@gmail.com>
2023-05-26 11:00:49 -0400

    fix loop variable bug

--- cli/commandmap.go
+++ cli/commandmap.go
@@ -11,11 +11,12 @@ func commandsToMap(commands []command.Command, prefix string) map[string]func(ar
 	commandMap := make(map[string]func(args []string))
 	for _, command := range commands {
 		key := prefix + command.Name
+		cmdFunc := command.Func
 		commandMap[key] = func(args []string) {
-			command.Func(args)
+			cmdFunc(args)
 		}
 		if len(command.SubCommands) > 0 {
-			nestedCommandMap := commandsToMap(command.SubCommands, key+"+")
+			nestedCommandMap := commandsToMap(command.SubCommands, key+".")
 			commandMap = mergeMaps(commandMap, nestedCommandMap)
 		}
 	}
